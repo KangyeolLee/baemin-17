6b54ededa16c06acbaba2190d61b3fd4e6976465
KangyeolLee <48883344+KangyeolLee@users.noreply.github.com>
2021-07-07 10:58:28 +0900

    [Feature/signup-detail] 회원가입-상세 페이지 기본 레이아웃 구성 및 경로 추가 (#21)
    * 상세 회원가입 페이지에서 사용할 이미지(되돌아가기 버튼) 추가
    
    * 상세 회원정보 페이지 렌더링을 위한 라우팅 경로(/signup/detail) 추가
    
    * 상세 회원가입 페이지 기본 마크업 구조 및 스타일링 작성
    
    * 헤더 통일성을 위해 이미지 버튼 클래스명 재지정
    
    * 기본 레이아웃 수정 - 상세 회원가입 페이지를 위한 외부 CSS link 추가 및 헤더 스타일링 상위 CSS 파일로 이동
    
    * 헤더 스타일링 최상위로 이동한 이유로 인해 관련 스타일링 제거
    
    * 라우팅 경로 변경으로 인한 서버로부터 받아오는 이미지 경로 수정

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/agree", function (req, res, next) {
+router.get("/signup/agree", function (req, res, next) {
   res.render("agree");
 });
 
 router.get("/login", function (req, res, next) {
   res.render("login");
+});
+
+router.get("/signup/detail", function (req, res, next) {
+  res.render("signup-detail");
 });
 
 module.exports = router;
