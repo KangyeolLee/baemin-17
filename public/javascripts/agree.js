8a933528b00215730515425188e5bca10ee280f8
KangyeolLee <48883344+KangyeolLee@users.noreply.github.com>
2021-07-08 10:11:20 +0900

    [Feature/agreement-js] 약관 페이지 기획서 기능 요구사항 구현 (#25)
    * 약관 페이지 인풋 관련 체크 기능과 페이지 이동 기능 구현
    
    * 약관 페이지 기능 구현에 따른 기존 스타일링 변경 및 스크립트 태그 추가
    
    * 약관 페이지 기능 구현 자바스크립트 컴포넌트화
    
    * 약관 페이지 기능 컴포넌트화에 따른 레이아웃 재배치와 그에 따른 스타일링 셀럭터 재지정
    
    * 페이지 고유 id 정책에 따라 회원가입-상세정보 페이지의 최상단 요소 아이디값 변경

---
+++ public/javascripts/agree.js
@@ -0,0 +1,78 @@
+function Agreement($agreement) {
+  this.$inputWrapper = $agreement.querySelector(".input-wrapper");
+  this.$allAgreeInput = $agreement.querySelector(".all-agree");
+  this.$restAgreeInputs = $agreement.querySelectorAll(
+    '.input-wrapper .rule-area input[type="checkbox"]'
+  );
+  this.$rulesAgreeInputs = $agreement.querySelectorAll(
+    '.rule-area input[type="checkbox"]'
+  );
+  this.$mustSelectedInputs = $agreement.querySelectorAll(
+    '.rule-area input[type="checkbox"].must-selected'
+  );
+  this.$nextBtn = $agreement.querySelector(".button");
+
+  this.arrayRestAgreeInputs = Array.from(this.$restAgreeInputs);
+  this.arrayRulesAgreeInputs = Array.from(this.$rulesAgreeInputs);
+  this.arrayMustSelectedInputs = Array.from(this.$mustSelectedInputs);
+
+  // 가입약관 페이지 관련 초기화 메서드
+  this.init = function () {
+    this.$allAgreeInput.addEventListener("click", () => this.toggleCheckAll());
+    this.$inputWrapper.addEventListener("click", () =>
+      this.checkEveryRuleInput()
+    );
+    this.$nextBtn.addEventListener("click", () => this.moveNextPage());
+  };
+
+  // 전체동의 클릭 시 모든 약관 항목 체크 메서드
+  this.toggleCheckAll = function () {
+    if (this.$allAgreeInput.checked) {
+      this.arrayRestAgreeInputs.forEach((input) => (input.checked = true));
+    } else {
+      this.arrayRestAgreeInputs.forEach((input) => (input.checked = false));
+    }
+
+    this.activeNextBtn();
+  };
+
+  // 전체동의 버튼 활성화 여부 체크 메서드
+  this.checkEveryRuleInput = function () {
+    const allChecked = this.arrayRulesAgreeInputs.every(
+      (input) => input.checked === true
+    );
+
+    if (!allChecked) {
+      this.$allAgreeInput.checked = false;
+    }
+
+    this.activeNextBtn();
+  };
+
+  // 다음 버튼 활성화 메서드
+  this.activeNextBtn = function () {
+    const allChecked = this.arrayMustSelectedInputs.every(
+      (input) => input.checked === true
+    );
+
+    if (allChecked) {
+      this.$nextBtn.classList.add("active");
+    } else {
+      this.$nextBtn.classList.remove("active");
+    }
+  };
+
+  // 다음 페이지 경로 이동 메서드
+  this.moveNextPage = function () {
+    const activated = this.$nextBtn.className.includes("active");
+
+    if (activated) {
+      window.location.href = "/signup/detail";
+    }
+  };
+}
+
+const $agreement = document.getElementById("agreement-page");
+const agreementPageInstance = new Agreement($agreement);
+
+agreementPageInstance.init();
